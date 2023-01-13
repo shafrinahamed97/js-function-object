b7562ca178b5f3809737c64ce15bd276b0001502
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-01-13 17:28:04 +0600

    funtion example and function summary

---
+++ add.js
@@ -0,0 +1,35 @@
+function add(num1, num2){
+    const sum = num1 + num2;
+    return sum;
+}
+const result1 = add(12,13);
+const result2 = add(35,7);
+const finalResult = result1 + result2;
+
+/*
+  Imagine a situation, You are the youngest member in your family. Everyone call you all the 
+  time to perform their tasks. Ans your situation like :)
+
+*/
+
+/*
+  Imagine another situation, you are working in an industry. Everyone about 10k products are being made there and your
+  job is selling on products.
+  What will you do?
+  Can you complete this task on time?
+*/
+
+/*
+  So What is Function?
+  And: A Procedure
+      A set of smaller tasks that performs a bigger task when something call it.
+      Maintain a relationship with input (the raw products which is given a function to perform
+        task) and output (the ready product)
+*/
+
+/*
+   What is function in js?
+   Ans: A block of code.
+       A set of statement (subprogram/ block of code) that performs a task when it is called.
+       Maintains a relationship with input and output
+*/
