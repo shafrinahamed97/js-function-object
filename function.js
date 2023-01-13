034d432b331caf6e7f6899daf8ec23a921866193
shafrinahamed97 <shafrinahamedjob@gmail.com>
2023-01-13 11:35:14 +0600

    declare a function, call function

---
+++ function.js
@@ -0,0 +1,16 @@
+// ফাঙ্কশন কয়েক লাইন কোড এর সমস্ষ্টি (block of code)।  যেটা নিদ্দিষ্ট  একটা কাজ সম্পাদন করে .
+// ফাঙ্কশন লিখতে হলে প্রথেমেই function কীওয়ার্ড  লিখতে হবে, IT's must।  
+
+// function declaration
+function startFan(){ 
+    //ফাঙ্কশন কীওয়ার্ড এর পর  ফাঙ্কশন এর নাম লিখতে হয় 
+    // ফাঙ্কশন  এর নাম গুলো লেটার, ডিজিটস, আন্ডারস্কোর  এন্ড ডলার sign  থাকতে হবে 
+    // ফাঙ্কশন নামের পর parentheses () ব্যবহার  থাকতে হয় 
+    // parentheses এর পর  কারলি ব্র্যাকেট {} ব্যাবহার করতে হবে এন্ড কারলি ব্র্যাকেট এর ভিতরে কোড লিখতে হবে 
+    console.log('Stand Up');
+    console.log('walk towards the switch');
+    console.log('Press the switch');
+} 
+
+// call the function
+startFan();   
